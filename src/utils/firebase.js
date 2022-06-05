0d351ce20f0c3aca53b72cd8c3685935151e5f3b
Michal Tkaczuk <mchltkczk@gmail.com>
2022-06-05 15:46:25 +0200

    Folder strucutre ready

---
+++ src/utils/firebase.js
@@ -0,0 +1,6 @@
+import { config } from './config'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
