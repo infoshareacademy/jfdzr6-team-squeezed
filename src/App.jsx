01980e7f588576cfa30007dddf408343a81b2838
sylwia-laboszczak <sylwiajurczyk91@gmail.com>
2022-06-11 17:10:30 +0200

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import { SearchResults } from "./components/OffersList/SearchResults/SearchResul
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AddOffer } from "./components/ClientPanel/AddOffer/AddOffer";
 import { NavBarClientPanel } from "./components/ClientPanel/NavBarClientPanel/NavBarClientPanel";
+import { Home } from "./Routes/Home";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <SearchResults />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-      <NavBarClientPanel />
-      <div>
-        <Routes>
-          <Route path="newoffer" element={<AddOffer />}>
-            {/* </Routes>/<Route path="newoffer/:flatsId" element={<AddOffer />} /> */}
-            {/* <AddOffer /> */}
-          </Route>
-        </Routes>
-      </div>
+        <Route path="newoffer" element={<AddOffer />}>
+          {/* </Routes>/<Route path="newoffer/:flatsId" element={<AddOffer />} /> */}
+          {/* <AddOffer /> */}
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
