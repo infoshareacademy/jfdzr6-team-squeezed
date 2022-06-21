54c4a38d097a11ba97bddcdcaab4e24bacfb3e7d
Lidia Dalecka <lidia.dalecka@op.pl>
2022-06-22 00:25:27 +0200

    added NewOffer component

---
+++ src/Routes/NewOffer.jsx
@@ -0,0 +1,22 @@
+
+import { NavBarClientPanel } from "../components/ClientPanel/NavBarClientPanel/NavBarClientPanel";
+import { AddOffer } from "../components/ClientPanel/AddOffer/AddOffer";
+import { Footer } from '../components/Footer/Footer';
+
+export const NewOffer = () => {
+
+    return (
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+            }}>
+
+            <AddOffer />
+            <Footer></Footer>
+        </div>
+    );
+
+
+};
