39b6b65dbdb394ce4832eca69aa5b8adc72b51e1
Lidia Dalecka <lidia.dalecka@op.pl>
2022-06-22 00:33:13 +0200

    added Footer.styled component

---
+++ src/components/Footer/Footer.styled.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const SocialMedia = styled.div`
+  padding: 20px;
+  margin: 10px 10px;
+  color: black;
+  font-size: 30px;
+  display: flex;
+
+  flex-direction: row;
+  div {
+    margin: 0 20px;
+  }
+  svg {
+    color: white;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  background-color: #0975c3;
+  align-items: center;
+  justify-content: space-between;
+  padding-left: 20px;
+  flex-wrap: wrap;
+
+  @media (min-width: 768px) {
+    flex-wrap: nowrap;
+  }
+
+  p {
+    display: flex;
+    justify-content: flex-end;
+    color: white;
+    font-family: "Open Sans", sans-serif;
+    font-weight: bolder;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`;
